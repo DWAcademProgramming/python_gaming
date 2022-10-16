d36cf9b15e5f207c591c06e890daf6108e7011db
David <95488893+DWAcademProgramming@users.noreply.github.com>
2022-10-15 19:36:30 -0500

    Boredom === Making video games on a Saturday night...

--- feed_the_dragon.py
+++ feed_the_dragon.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame 
+import random
 
 #initialize pygame
 pygame.init()
@@ -47,19 +48,110 @@ lives_text = font.render("Lives: " + str(player_lives), True, GREEN, WHITE)
 lives_rect = lives_text.get_rect()
 lives_rect.topright = (WINDOW_WIDTH - 10, 10)
 
-#Set sounds and music
+game_over_text = font.render("Press any key to play again", True, GREEN, DARKGREEN)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 
+continue_text = font.render("Continue?", True, GREEN, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 +32)
+
+#Set sounds and music
+coin_sound = pygame.mixer.Sound("coin_sound.wav")
+miss_sound = pygame.mixer.Sound("miss_sound.wav")
+miss_sound.set_volume(.1)
+pygame.mixer.music.load("ftd_background_music.wav")
 
 #Set images
+play_image = pygame.image.load("dragon_right.png")
+player_rect = play_image.get_rect()
+player_rect.left = 32
+player_rect.centery = WINDOW_HEIGHT//2
 
+coin_image = pygame.image.load("coin.png")
+coin_rect = coin_image.get_rect()
+coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+coin_rect.y = random.randint(64, WINDOW_HEIGHT -32)
 
 #The main game loop
+pygame.mixer.music.play(-1, 0.0)
 running = True
 while running: 
     #If the user wants to quit
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             running = False
+    
+    #Check to see if the player wants to move
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and player_rect.top > 64:
+        player_rect.y -= PLAYER_VELOCITY
+    if keys[pygame.K_DOWN] and player_rect.top < WINDOW_HEIGHT:
+        player_rect.y += PLAYER_VELOCITY
+
+    #Move the coin
+    if coin_rect.x < 0: 
+        #Player missed the coin
+        player_lives -=1 
+        miss_sound.play()
+        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+        coin_rect.y = random.randint(64, WINDOW_HEIGHT - 32)
+    else: 
+        #Move the coin
+        coin_rect.x -= coin_velocity
+
+    #Check for collisions
+    if player_rect.colliderect(coin_rect): 
+        score += 1
+        coin_sound.play()
+        coin_velocity += COIN_ACCELERATION
+        coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+        coin_rect.y = random.randint(64, WINDOW_HEIGHT -32)
+
+    #Fill the display 
+    display_surface.fill(BLACK)
+
+    #Blit HUD to the screen
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(lives_text, lives_rect)
+    pygame.draw.line(display_surface, WHITE, (0, 64), (WINDOW_WIDTH, 64), 2)
+
+    #Update HUD
+    score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+    lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
+
+    #Check for game over
+    if player_lives ==0: 
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        pygame.display.update()
+        pygame.mixer.music.play(-1, 0.0)
+        is_paused = False
+
+        #Pause the game until the player presses a key, then reset the game
+        pygame.mixer.music.stop()
+        is_paused = True
+        while is_paused:
+            # If the player wants to play again
+            if event.type == pygame.KEYDOWN: 
+                score = 0
+                player_lives = PLAYER_STARTING_LIVES
+                player_rect.y = WINDOW_HEIGHT//2
+                coin_velocity = COIN_STARTING_VELOCITY
+                is_paused = False
+            #The player wants to quit
+            if event.type ==  pygame.QUIT:
+                is_paused = False
+                running = False
+
+    #Blit assets to the screen
+    display_surface.blit(play_image, player_rect)
+    display_surface.blit(coin_image, coin_rect)
+
+    #Update display and tick the clock
+    pygame.display.update()
+    clock.tick(FPS)
 
 #End the game
 pygame.quit()
